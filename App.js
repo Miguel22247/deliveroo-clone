e94b5bb5c0858f54fb5105ced5fe1ac83d299769
Miguel Pacheco <minipachru@gmail.com>
2024-03-13 19:16:25 -0300

    add basic files

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
+
+// nativewind approach
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 justify-center items-center bg-blue-500 ">
+      <TouchableOpacity className="bg-teal-500 p-3 rounded-lg shadow-md shadow-gray-400">
+        <Text className="text-white text-3xl font-bold ">Hello World</Text>
+      </TouchableOpacity>
+      
+      <StatusBar style="dark" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+// // twrnc approach
+// export default function App() {
+//   return (
+//     <View style={tw`flex-1 justify-center items-center bg-white`}>
+//       <TouchableOpacity style={tw`bg-teal-500 p-3 rounded-l`}>
+//         <Text style={tw`text-white text-3xl font-bold`}>Hello World</Text>
+//       </TouchableOpacity>
+      
+//       <StatusBar style="dark" />
+//     </View>
+//   );
+// }
