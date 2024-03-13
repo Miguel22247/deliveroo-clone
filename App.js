6c1e02e83b6d2c92e9fc84adcf968660f9660b95
Miguel Pacheco <minipachru@gmail.com>
2024-03-13 19:27:43 -0300

    remove tw approach

--- App.js
+++ App.js
@@ -14,17 +14,3 @@ export default function App() {
     </View>
   );
 }
-
-
-// // twrnc approach
-// export default function App() {
-//   return (
-//     <View style={tw`flex-1 justify-center items-center bg-white`}>
-//       <TouchableOpacity style={tw`bg-teal-500 p-3 rounded-l`}>
-//         <Text style={tw`text-white text-3xl font-bold`}>Hello World</Text>
-//       </TouchableOpacity>
-      
-//       <StatusBar style="dark" />
-//     </View>
-//   );
-// }
