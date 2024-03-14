9a42005eb86b2fbec9c3a92b6c97d2468407af49
Miguel Pacheco <minipachru@gmail.com>
2024-03-14 10:04:47 -0300

    add new screens

--- screens/Homepage.jsx
+++ screens/Homepage.jsx
@@ -1,14 +1,10 @@
-import { Text, SafeAreaView, View, Image, TextInput } from "react-native";
+import { Text, SafeAreaView, View, Image, ScrollView } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
-import {
-  UserIcon,
-  ChevronDownIcon,
-  MagnifyingGlassIcon,
-  AdjustmentsVerticalIcon,
-} from "react-native-heroicons/outline";
-import Header from "../components/Header";
-import Search from "../components/Search";
+import Header from "../components/common/Header";
+import Search from "../components/common/Search";
+import Categories from "../components/Categories";
+import FeaturedRow from "../components/FeaturedRow";
 
 const Homepage = () => {
   const navigation = useNavigation();
@@ -25,6 +21,33 @@ const Homepage = () => {
       <Header />
       {/* Search */}
       <Search />
+
+      {/* Body */}
+      <ScrollView className="bg-gray-100">
+        {/* Categories */}
+        <Categories />
+
+        {/* Offers Near You */}
+        <FeaturedRow
+          id="123"
+          title="Offers Near you"
+          description="Why not support your local restaurant tonight!"
+        />
+
+        {/* Featured */}
+        <FeaturedRow
+          id="1234"
+          title="Featured"
+          description="Paid placements from our partners"
+        />
+
+        {/* Tasty Discounta */}
+        <FeaturedRow
+          id="12345"
+          title="Tasty Discounts"
+          description="Everyone's been enjoying these juicy discounts"
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 };
