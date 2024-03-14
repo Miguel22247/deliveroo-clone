9851b4e172f41495b0865396721d87ea5350d1ef
Miguel Pacheco <minipachru@gmail.com>
2024-03-14 08:11:32 -0300

    add App file as jsx and Homepage file

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import { Button, View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Homepage from "./screens/Homepage";
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Homepage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
