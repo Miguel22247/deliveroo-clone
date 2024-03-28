6f256215eb20040795b7c18cbc2498cb35e263b9
Miguel Pacheco <minipachru@gmail.com>
2024-03-28 09:41:46 -0300

    fix: colors and update icons

--- screens/RestaurantScreen.jsx
+++ screens/RestaurantScreen.jsx
@@ -3,9 +3,10 @@ import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import {
   ArrowLeftIcon,
   StarIcon,
-  QuestionMarkCircleIcon,
+  MapPinIcon,
+  ChevronRightIcon,
 } from "react-native-heroicons/solid";
-import { MapPinIcon, ChevronRightIcon } from "react-native-heroicons/outline";
+import { QuestionMarkCircleIcon } from "react-native-heroicons/outline";
 import { useNavigation } from "@react-navigation/native";
 import { useRoute } from "@react-navigation/native";
 import { urlFor } from "../sanity";
@@ -66,15 +67,15 @@ const RestaurantScreen = () => {
           </View>
 
           <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
-
-          <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
-            <QuestionMarkCircleIcon color="gray" opacity={0.6} size={20} />
-            <Text className="pl-2 flex-1 text-md font-bold">
-              Have a food allergy?
-            </Text>
-            <ChevronRightIcon color="#00CCBB" />
-          </TouchableOpacity>
         </View>
+
+        <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
+          <QuestionMarkCircleIcon color="gray" opacity={0.6} size={20} />
+          <Text className="pl-2 flex-1 text-md font-bold">
+            Have a food allergy?
+          </Text>
+          <ChevronRightIcon color="#00CCBB" />
+        </TouchableOpacity>
       </View>
 
       <View>
